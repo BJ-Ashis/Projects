5fd090963784f30c923a0a48137b996147f82a02
Ashis Kumar Panda <ashiskumarpanda727@gmail.com>
2025-03-04 01:45:27 +0530

    Delete 2. Expense App directory

--- 2. Expense App/Back-End/src/main/java/com/example/spring/exceptions/ItemExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.example.spring.exceptions;
-
-public class ItemExistsException extends RuntimeException{
-
-    public ItemExistsException(String message) {
-        super(message);
-    }
-}
--- 2. Expense App/Front-End/src/model/Expense.ts
+++
@@ -1,9 +0,0 @@
-export interface Expense {
-    id?: number;
-    expenseId?: string;
-    name: string;
-    note: string;
-    amount: string;
-    date: string;
-    category: string;
-}
