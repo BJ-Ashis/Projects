bd60d9b38e37c16247bbc39c936dc9bf91c80ef9
Ashis Kumar Panda <ashiskumarpanda727@gmail.com>
2025-03-04 17:47:21 +0530

    Add files via upload

---
+++ 02. Expense App/Front-End/src/services/expense-service.ts
@@ -0,0 +1,21 @@
+import apiClient from "../config/api-client"
+import { Expense } from "../model/Expense"
+
+export const getExpenses = () => {
+    return apiClient.get<Expense[]>('/expenses');
+}
+
+export const getExpenseByExpenseId = (expenseId: string) => {
+    return apiClient.get<Expense>(`/expenses/${expenseId}`);
+}
+
+export const deleteExpenseByExpenseId = (expenseId: string) => {
+    return apiClient.delete<void>(`/expenses/${expenseId}`);
+}
+
+export const saveOrUpdateExpense = (expense: Expense) => {
+    if (expense.expenseId !== undefined || expense.expenseId != null) {
+        return apiClient.put<Expense>(`/expenses/${expense.expenseId}`, expense);
+    }
+    return apiClient.post<Expense>(`/expenses`, expense);
+}
